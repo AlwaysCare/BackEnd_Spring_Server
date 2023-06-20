a19fe53e65a763342a8e5011e5a6d04adb0bd05b
Kimhyunwoo-computer <rlagusdn3046@naver.com>
2023-06-20 23:24:38 +0900

    서버 구축

---
+++ src/main/java/server/AlwaysCare/AlwaysCareApplication.java
@@ -0,0 +1,13 @@
+package server.AlwaysCare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlwaysCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlwaysCareApplication.class, args);
+	}
+
+}
